caa2fb3cb18d43746b7b289fc89cb395c433f95a
Ken Cheng <kencheng@github.com>
2026-05-26 00:37:44 +0800

    Fix: char-select timeout overwrite + renderBattleAnswer order (currentQuestion null)
    Bug 1: selectChar() setTimeout was overwriting showMapScreen when user fast-forwarded through char screen
    Bug 2: renderBattleAnswer() called before nextQuestion() → currentQuestion was null → choices not rendered
    
    Fix 1: clearTimeout before setTimeout in selectChar
    Fix 2: swap renderBattleAnswer() and nextQuestion() order in startBattle()

--- src/engine/index.js
+++
@@ -1,4 +0,0 @@
-// engine/index.js - Export all engine modules
-export { GameEngine, createGameEngine } from './GameEngine.js';
-export { GameBridge, createGameBridge, getBridge, initBridge } from './GameBridge.js';
-export { NetworkClient, createNetworkClient } from './NetworkClient.js';
--- src/managers/index.js
+++
@@ -1,3 +0,0 @@
-// managers/index.js - Export all managers
-export { SaveManager, createSaveManager } from './SaveManager.js';
-export { LocalSessionManager, createLocalSessionManager } from './LocalSessionManager.js';
